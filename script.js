07aa66818f9ad2d035d0300b523ea64ef852ac30
Devansh0309 <93301328+Devansh0309@users.noreply.github.com>
2023-09-02 14:06:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+function query() {
+	alert("hello")
+	const name = document.getElementById("name").value
+	const year = document.getElementById("year").value
+	// alert(name)
+	
+	if(name && year){
+		document.getElementById("url").innerText="https://localhost:8080/"+"?name="+name+"&year="+year
+	}
+	else if(name && !year){
+		document.getElementById("url").innerText="https://localhost:8080/"+"?name="+name
+	}
+	else if(!name && year){
+		document.getElementById("url").innerText="https://localhost:8080/""?year="+year
+	}
+	else{
+		document.getElementById("url").innerText="https://localhost:8080/"
+	}
+	alert(document.getElementById("url").innerText)
+	
+}
+query()
